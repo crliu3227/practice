e7ebf676eef4d21cdd8e4dc0f247cfa2e2284b34
LCR <liucongran327@163.com>
2018-06-21 20:29:57 +0800

    dictsort

---
+++ dictsort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# 题目描述
+# 给定n个字符串，请对n个字符串按照字典序排列。
+# 输入描述:
+
+# 输入第一行为一个正整数n(1≤n≤1000),下面n行为n个字符串(字符串长度≤100),字符串中只含有大小写字母。
+
+# 输出描述:
+
+# 数据输出n行，输出结果为按照字典序排列的字符串。
+
+
+def main():
+    num = int(input())
+    inputstring = []
+    for n in range(num):
+        inputstring.append(input())
+
+    for s in sorted(inputstring):
+        print(s)
+
+
+if __name__ == '__main__':
+    main()
